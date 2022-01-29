4cbc46508f319bc0843c6bffe491e1429bb06fee
Nathan Flessner <nathanflessner1@gmail.com>
2022-01-29 15:23:09 -0700

    added filterkey and began working on tabs being shown

--- src/components/VisibilityFilter.js
+++ src/components/VisibilityFilter.js
@@ -1,10 +1,19 @@
+import { Container, Tab, TabList, Tabs } from '@chakra-ui/react';
+import object from 'prop-types'
 import React from 'react';
+import { VISIBILITY_FILTER } from '../constants';
 
 const VisibilityFilter = () => {
   return (
-    <div>
-      nav filter
-    </div>
+    <Container centerContent>
+      <Tabs>
+        <TabList>
+          {object.keys(VISIBILITY_FILTER).map(filterkey => {
+            const currentFilter = VISIBILITY_FILTER[filterkey];
+          })}
+        </TabList>
+      </Tabs>
+    </Container>
   );
 };
 
