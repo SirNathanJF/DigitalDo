2f3c11f68ce63ea24088d4227a241a405db8350c
Nathan Flessner <nathanflessner1@gmail.com>
2022-02-01 14:39:36 -0700

    created store for redux, added it to main index.js

--- src/index.js
+++ src/index.js
@@ -2,15 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
+import { Provider } from "react-redux";
+import store from "./redux/store";
 // Chakra UI
 import { ChakraProvider } from "@chakra-ui/react";
 
 ReactDOM.render(
-  <ChakraProvider>
+  <Provider store={store}>
+      <ChakraProvider>
     <React.StrictMode>
       <App />
     </React.StrictMode>
     ,
   </ChakraProvider>,
+  </Provider>,
+
   document.getElementById("root")
 );
