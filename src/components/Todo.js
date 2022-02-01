40709c74de4a0145d77311c85caed3de6a4f9e95
Nathan Flessner <nathanflessner1@gmail.com>
2022-02-01 15:40:55 -0700

    used useEffect to set the checkboxes to be checked when clicked

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,14 +1,18 @@
 import { Box, Checkbox, Text } from '@chakra-ui/react';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { toggleTodo } from '../redux/actions';
 
 
 export const Todo = ({ todo }) => {
   const dispatch = useDispatch();
-  const [checked, isChecked] = useState(false);
+  const [checked, setChecked] = useState(false);
 
   const handleCheck = () => dispatch(toggleTodo(todo.id))
+
+  useEffect(() => {
+    setChecked(todo.completed)
+  },[todo]);
 
   return (
     <Box mb={1} bgColor={!todo.completed ? 'lightcoral' : "lightgreen"} p={2}>
