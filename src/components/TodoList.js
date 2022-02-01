919e20258262168c477ec73ab3b9d05b3ac2aad3
Nathan Flessner <nathanflessner1@gmail.com>
2022-02-01 16:02:26 -0700

    set up visibilityFilter to be able to select tabs that the user wishes to view their tasks

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,13 +1,16 @@
-import { Box } from "@chakra-ui/react";
+import { Box, filter } from "@chakra-ui/react";
 import React from "react";
 import { useSelector } from "react-redux";
+import { getTodosByVisibilityFilter } from "../redux/selector";
 import { Todo } from "./Todo";
 
 const TodoList = () => {
-  const { todos } = useSelector((state) => state);
+  const { todos, visibilityFilter } = useSelector((state) => state);
+  const filterTodos = getTodosByVisibilityFilter(todos, visibilityFilter);
+
   return (
     <Box my={3}>
-      {todos.todos.length ? todos.todos.map(todo => (
+      {filterTodos.length ? filterTodos.map(todo => (
         <Todo key={`todo-${todo.id}`} todo={todo}/>
       )):<Box textAlign="center" my="4">
           No tasks found, great job!
--- src/components/VisibilityFilter.js
+++ src/components/VisibilityFilter.js
@@ -1,8 +1,13 @@
 import { Container, Tab, TabList, Tabs } from "@chakra-ui/react";
 import React from "react";
+import { useDispatch } from "react-redux";
 import { VISIBILITY_FILTER } from "../constants";
+import { setFilter } from "../redux/actions";
 
 const VisibilityFilter = () => {
+
+  const dispatch = useDispatch();
+  
   return (
     <Container centerContent>
       <Tabs>
@@ -10,7 +15,7 @@ const VisibilityFilter = () => {
           {Object.keys(VISIBILITY_FILTER).map((filterkey) => {
             const currentFilter = VISIBILITY_FILTER[filterkey];
             return (
-              <Tab key={`visibility-filter-${currentFilter}`}>
+              <Tab key={`visibility-filter-${currentFilter}`} onClick={() => dispatch(setFilter(currentFilter))}>
                 {currentFilter}
               </Tab>
             );
